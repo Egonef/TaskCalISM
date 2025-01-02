f06578d675263c3da8c94ea6051dac1b1f2480e7
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2025-01-02 20:04:28 +0100

    mhghj

--- frontend/routes/Notifications.js
+++ frontend/routes/Notifications.js
@@ -18,19 +18,21 @@ export default function Notifications() {
     const [notifications, setNotifications] = useState([]);
 
     //Funcion para obtener las notificaciones
-    const getNotifications = async () => {
-        const userInfo = await AsyncStorage.getItem('userInfo');
-        const userID = JSON.parse(userInfo)._id;
-        try {
-            const res = await axios.get(`${BACKEND_IP}/api/notification/${userID}`);
-            setNotifications(res.data);
-        }catch (error) {
-            console.error('Error creating lists:', error);
-        }
-    }
-
-
     useEffect(() => {
+        const getNotifications = async () => {
+            const userInfo = await AsyncStorage.getItem('userInfo');
+            const userID = JSON.parse(userInfo)._id;
+            console.log("Entrando a getNotifications");
+            try {
+                const res = await axios.get(`${BACKEND_IP}/api/notification/${userID}`);
+                console.log('Notifications:', res.data);
+                setNotifications(res.data);
+                console.log('Notifications guardadas:', notifications);
+            }catch (error) {
+                console.error('Error creating lists:', error);
+            }
+        }
+
         getNotifications();
     },[]);
 
@@ -39,12 +41,12 @@ export default function Notifications() {
         <View style={styles.container}>
             <StatusBar style="auto" />
             <Text style={styles.header}>Your Inbox</Text>
-            <View>
+            <View style={styles.notificationContainer}>
                 <FlatList
                     data={notifications}
                     keyExtractor={(item) => item._id}
                     renderItem={({ item }) => (
-                        <View>
+                        <View style={styles.notificationCard}>
                             <Text>{item.title}</Text>
                             <Text>{item.description}</Text>
                         </View>
@@ -69,5 +71,21 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         margin: 20,
+    },
+    notificationContainer: {
+        width: '80%',
+        height: '70%',
+        backgroundColor: 'lightgrey',
+        padding: 10,
+        margin: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+    notificationCard: {
+        backgroundColor: 'white',
+        padding: 10,
+        margin: 10,
+        borderWidth: 1,
+        borderColor: 'black',
     },
 });
