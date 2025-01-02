f60801eaaff9fe80599dfcaf8174a2108c680429
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2025-01-02 20:13:17 +0100

    FUNCIONAAAAAAAAA mañana le pongo un boton de aceptar

--- frontend/routes/Notifications.js
+++ frontend/routes/Notifications.js
@@ -47,8 +47,10 @@ export default function Notifications() {
                     keyExtractor={(item) => item._id}
                     renderItem={({ item }) => (
                         <View style={styles.notificationCard}>
-                            <Text>{item.title}</Text>
-                            <Text>{item.description}</Text>
+                            <Text style={styles.notificationTitle}>
+                                {item.titulo}
+                            </Text>
+                            <Text>{item.descripcion}</Text>
                         </View>
                     )}
                 />
@@ -73,19 +75,26 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     notificationContainer: {
-        width: '80%',
-        height: '70%',
-        backgroundColor: 'lightgrey',
+        width: '100%',
+        height: '80%',
         padding: 10,
         margin: 10,
-        borderWidth: 1,
-        borderColor: 'black',
     },
     notificationCard: {
-        backgroundColor: 'white',
+        borderRadius: 10,
+        height: 150,
+        backgroundColor: '#B5C18E',
         padding: 10,
         margin: 10,
-        borderWidth: 1,
-        borderColor: 'black',
+        borderColor: '#B4A593',
+        borderWidth: 2,
+    },
+    notificationTitle: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'black',
+    },
+    notificationDescription: {
+        fontSize: 14,
     },
 });
